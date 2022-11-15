e3e71fc55e050fbdd59de30a0c8f1db3f369fd48
Tung2709 <vuthanhtungautomation@gmail.com>
2022-11-15 16:15:28 +0700

    update icon heart in Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,31 +5,39 @@ import { getProductApi } from '../../redux/productReducer/productReducer'
 import { Carousel } from "antd";
 import 'antd/dist/antd.min.css'
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import { getProductFavoriteApi } from '../../redux/productReducer/userReducer';
 
 export default function Home() {
   const dispatch = useDispatch()
   const { arrProduct } = useSelector(state => state.productReducer)
+  const {userProductsFavorite} = useSelector(state => state.userReducer)
+  console.log('cái kết',userProductsFavorite)
   useEffect(() => {
     const action = getProductApi();
+    dispatch(action);
+  }, [])
+  const addProductFavorite =(prod)=>{
+    const action = getProductFavoriteApi(prod);
     dispatch(action)
-  },[])
+  }
   return (
     <div>
-      <div className='carousel w-100' style={{padding:'76px 0px 101px'}} >
-      <Carousel arrows prevArrow={<LeftOutlined />}nextArrow={<RightOutlined />}>
-      {arrProduct.map((prod,index)=>{
-        return <div className='d-flex flex-row justify-content-between' key={index}> 
-        <div className=" h-100" style={{paddingLeft:102,width:'70%'}} >
-            <img src={prod.image} style={{width:689,height:383,objectFit:'cover'}} alt="..." />
-          </div>
-          <div className=" product-detail" style={{paddingTop:48,width:'30%'}}>
-          <h3>{prod.name}</h3>
-                  <p>{prod.description}</p>
-                  <NavLink to={`detail/${prod.id}`} className="btn">Buy now </NavLink>
-          </div>
-          </div>
-      })}
-    </Carousel>
+      <div className='carousel w-100' style={{ padding: '76px 0px 101px' }} >
+        <Carousel arrows prevArrow={<LeftOutlined />} nextArrow={<RightOutlined />}>
+          {arrProduct.map((prod, index) => {
+            return <div className='d-flex flex-row justify-content-between' key={index}>
+              <div className=" h-100" style={{ paddingLeft: 102, width: '70%' }} >
+                <img src={prod.image} style={{ width: 689, height: 383, objectFit: 'cover' }} alt="..." />
+              </div>
+              <div className=" product-detail" style={{ paddingTop: 48, width: '30%' }}>
+                <h3>{prod.name}</h3>
+                <p>{prod.description}</p>
+                <NavLink to={`detail/${prod.id}`} className="btn">Buy now </NavLink>
+              </div>
+            </div>
+          })}
+        </Carousel>
+
       </div>
       <div className="product-feature">
         <h1>Product Feature</h1>
@@ -37,16 +45,19 @@ export default function Home() {
           {arrProduct.map((prod, index) => {
             return <div className="col-4" key={index}>
               <div className="card">
-              <i className="fa-regular fa-heart"></i>
+                <i className="fa-regular fa-heart" style={{display:userProductsFavorite.filter(item=>item===prod.id).length===0?'':'none'}} onClick={()=>{addProductFavorite(prod)}}>      
+                 </i>
+                 <i className="fa-solid fa-heart" style={{display:userProductsFavorite.filter(item=>item===prod.id).length===0?'none':''}} onClick={()=>{addProductFavorite(prod)}}>      
+                 </i>
                 <img src={prod.image} alt="..." />
                 <div className="card-body">
                   <h3>{prod.name}</h3>
                   <p>{prod.shortDescription}</p>
                 </div>
-                  <div className="card-footer d-flex">
+                <div className="card-footer d-flex">
                   <NavLink to={`detail/${prod.id}`} className="btn">Buy now</NavLink>
                   <p >{prod.price}$</p>
-                  </div>
+                </div>
               </div>
             </div>
           })}
--- src/redux/productReducer/userReducer.jsx
+++ src/redux/productReducer/userReducer.jsx
@@ -3,8 +3,9 @@ import 'axios'
 import { ACCESS_TOKEN, http, setting, USER_LOGIN } from '../../util/config';
 const initialState = {
 	//nếu localStorage có dữ liệu => load dữ liệu default cho state.userLogin của redux, nếu localStorage không có thì gán object {}
-	userLogin: setting.getStorageJSON(USER_LOGIN)? setting.getStorageJSON(USER_LOGIN):{},
-	userProfile:{}
+	userLogin: setting.getStorageJSON(USER_LOGIN) ? setting.getStorageJSON(USER_LOGIN) : {},
+	userProfile: {},
+	userProductsFavorite: [],
 }
 
 const userReducer = createSlice({
@@ -12,16 +13,31 @@ const userReducer = createSlice({
 	initialState,
 	reducers: {
 		loginAction: (state, action) => {
-				const userLogin = action.payload;
-				state.userLogin= userLogin
+			const userLogin = action.payload;
+			state.userLogin = userLogin
 		},
-		getProfileAction:(state,action)=>{
-			state.userProfile=action.payload;
+		getProfileAction: (state, action) => {
+			state.userProfile = action.payload;
 		}
+		,
+		getProductFavoriteAction:  (state, action) => {
+			const addProductFavorite = action.payload
+			if(state.userProductsFavorite.filter(user=> user===addProductFavorite.productsFavorite).length===0){
+				console.log('add')
+				state.userProductsFavorite.push(addProductFavorite.productsFavorite)
+				console.log('đây nè',state.userProductsFavorite)
+			}else{
+				console.log('remove')
+				state.userProductsFavorite=state.userProductsFavorite.filter(user => user!==addProductFavorite.productsFavorite)
+				console.log('đây nè',state.userProductsFavorite)
+			}
+			
+		}
+		,
 	}
 });
 
-export const { loginAction,getProfileAction } = userReducer.actions
+export const { loginAction, getProfileAction, getProductFavoriteAction } = userReducer.actions
 
 export default userReducer.reducer
 
@@ -37,19 +53,19 @@ export const loginApi = (userLogin) => {
 		const actionGetProfile = getProfileApi()
 		dispatch(actionGetProfile)
 		// sau khi đăng nhập thành công thì lưu vào localStorage và cookie
-		setting.setStorageJSON(USER_LOGIN,result.data.content)
-		setting.setStorage(ACCESS_TOKEN,result.data.content.accessToken)
-		setting.setCookie(ACCESS_TOKEN,result.data.content.accessToken,30)
+		setting.setStorageJSON(USER_LOGIN, result.data.content)
+		setting.setStorage(ACCESS_TOKEN, result.data.content.accessToken)
+		setting.setCookie(ACCESS_TOKEN, result.data.content.accessToken, 30)
 	}
 }
 // cách 1 truyền navigate để chuyển hướng trang khi chưa đăng nhập
 // cách 2 cấu hình bên trong interceptor
 export const getProfileApi = () => {
-	return async dispatch =>{
+	return async dispatch => {
 		// try{
-			const result = await http.post('/api/Users/getProfile')
-			const action = getProfileAction(result.data.content)
-			dispatch(action)
+		const result = await http.post('/api/Users/getProfile')
+		const action = getProfileAction(result.data.content)
+		dispatch(action)
 		// } catch(err){
 		// 	if(err?.response?.status === 401 ){
 		// 		alert('Đặng nhập')
@@ -59,9 +75,9 @@ export const getProfileApi = () => {
 	}
 }
 
-export const loginFacebook=(tokenFBApp)=>{
-	return async dispatch =>{
-		const result = await http.post('/api/Users/facebooklogin',{facebookToken: tokenFBApp});
+export const loginFacebook = (tokenFBApp) => {
+	return async dispatch => {
+		const result = await http.post('/api/Users/facebooklogin', { facebookToken: tokenFBApp });
 		const action = loginAction(result.data.content)
 		await dispatch(action)
 		// dispatch lại logic của 1 action async
@@ -69,9 +85,20 @@ export const loginFacebook=(tokenFBApp)=>{
 		const actionGetProfile = getProfileApi()
 		dispatch(actionGetProfile)
 		// sau khi đăng nhập thành công thì lưu vào localStorage và cookie
-		setting.setStorageJSON(USER_LOGIN,result.data.content)
-		setting.setStorage(ACCESS_TOKEN,result.data.content.accessToken)
-		setting.setCookie(ACCESS_TOKEN,result.data.content.accessToken,30)
+		setting.setStorageJSON(USER_LOGIN, result.data.content)
+		setting.setStorage(ACCESS_TOKEN, result.data.content.accessToken)
+		setting.setCookie(ACCESS_TOKEN, result.data.content.accessToken, 30)
 	}
 }
 
+export const getProductFavoriteApi = (prod) => {
+	return async dispatch => {
+		const result = await http.get('/api/Users/getproductfavorite');
+		const productFavorite = result.data.content.productsFavorite
+		if (productFavorite.filter(item => item === prod.id).length === 0) {
+			result.data.content.productsFavorite=prod.id
+		}
+		const action = getProductFavoriteAction(result.data.content)
+		dispatch(action)
+	}
+}
