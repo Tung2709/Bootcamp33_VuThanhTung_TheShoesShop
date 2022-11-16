54a85610b7e1edda740d687e0cf4b653af1bf56a
Tung2709 <vuthanhtungautomation@gmail.com>
2022-11-16 12:52:28 +0700

    complete Detail.jsx

--- src/Pages/Detail/Detail.jsx
+++ src/Pages/Detail/Detail.jsx
@@ -1,18 +1,24 @@
-import { Button } from 'antd';
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { NavLink, useParams } from 'react-router-dom';
 import { getProductDetailApi } from '../../redux/productReducer/productReducer';
+import { getProductFavoriteApi } from '../../redux/productReducer/userReducer';
+import { setting, USER_PRODUCTS_FAVORITE } from '../../util/config';
 
 export default function Detail() {
   const dispatch = useDispatch();
   const { id } = useParams()
   const { productDetail } = useSelector(state => state.productReducer)
-
+  const {userProductsFavorite} = useSelector(state => state.userReducer)
   useEffect(() => {
     const action = getProductDetailApi(id)
     dispatch(action)
   }, [id])
+
+  const addProductFavorite =(prod)=>{
+    const action = getProductFavoriteApi(prod);
+    dispatch(action)
+  }
 
   return (
     <div className='detail'>
@@ -24,9 +30,9 @@ export default function Detail() {
           <h3>{productDetail.name}</h3>
           <p>{productDetail.description}</p>
           <h4>Available size</h4>
-          {productDetail.size?.map((size,index)=>{
+          {productDetail.size?.map((size, index) => {
             return <div className="btn size" key={index} >
-                {size}
+              {size}
             </div>
           })}
           <h2>{productDetail.price}$</h2>
@@ -36,24 +42,32 @@ export default function Detail() {
           <br />
           <div className="btn add">Add to cart</div>
         </div>
-        
+
       </div>
       <div className="item-related">
-      <h3 >Related Products</h3>
-      <div className="row">
-        {productDetail.relatedProducts?.map((prod, index) => {
-          return <div className="col-4" key={index}>
-            <div className="card">
-              <img src={prod.image} alt="..." />
-              <div className="card-body">
-                <h3>{prod.name}</h3>
-                <p>{prod.price}</p>
-                <NavLink to={`/detail/${prod.id}`} className='btn btn-warning'>Add to cart <i className="fa fa-cart-plus"></i></NavLink>
+        <h3 >Related Products</h3>
+        <div className="row">
+          {productDetail.relatedProducts?.map((prod, index) => {
+            return <div className="col-4" key={index}>
+              <div className="card">
+              <i className="fa-regular fa-heart" style={{display:userProductsFavorite.filter(item=>Number(item)===prod.id).length===0?'':'none'}} onClick={()=>{addProductFavorite(prod)}}>      
+                 </i>
+                 { console.log(setting.getStorage(USER_PRODUCTS_FAVORITE))}
+                 <i className="fa-solid fa-heart" style={{display:userProductsFavorite.filter(item=>Number(item)===prod.id).length===0?'none':''}} onClick={()=>{addProductFavorite(prod)}}>      
+                 </i>
+                <img className='card-img-top' src={prod.image} alt="..." />
+                <div className="card-body">
+                  <h3>{prod.name}</h3>
+                  <p>{prod.shortDescription}</p>
+                </div>
+                <div className="card-footer d-flex">
+                  <NavLink to={`/detail/${prod.id}`} className='btn'>Buy now</NavLink>
+                  <p>{prod.price}$</p>
+                </div>
               </div>
             </div>
-          </div>
-        })}
-      </div>
+          })}
+        </div>
       </div>
     </div>
   )
